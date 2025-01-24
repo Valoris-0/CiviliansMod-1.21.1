e0bca3ac4611f0366a9e3d71df9b5c2e99ac7b85
AsianSandwiich <cweatherly4@yahoo.com>
2025-01-24 02:22:22 -0600

    cleaned code

--- src/main/java/net/asian/civiliansmod/ModSounds.java
+++ src/main/java/net/asian/civiliansmod/ModSounds.java
@@ -2,15 +2,9 @@ package net.asian.civiliansmod;
 
 import net.minecraft.sound.SoundEvent;
 import net.minecraft.util.Identifier;
-import net.minecraft.registry.Registries; // Correct imports
-import net.minecraft.registry.Registry; // Correct imports
+
 
 public class ModSounds {
     public static final Identifier NPC_CONVERSION_ID = Identifier.of("civiliansmod", "npc_conversion");
     public static final SoundEvent NPC_CONVERSION_SOUND = SoundEvent.of(NPC_CONVERSION_ID);
-
-    public static void registerSounds() {
-        // Register the sound event using SoundEvent registry
-        Registry.register(Registries.SOUND_EVENT, NPC_CONVERSION_ID, NPC_CONVERSION_SOUND);
-    }
 }
